44d6be4313e76dae642ec60eba4995cae7ca1203
tiwariParth <243mjparth@gmail.com>
2024-05-25 08:56:23 +0530

    chore: Add framer-motion and sass dependencies, update App component layout

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import "./navbar.scss";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <span>Mukesh Choudhary</span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
